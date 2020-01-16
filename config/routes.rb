b57d137cb0ada7e5beaf2a145964e2420ad70b8a
Jean Saglio <jean.saglio@gmail.com>
2020-01-16 13:17:24 +0100

    forgot to commit routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :matrices do
-        resources :tasks, only: [:index]
+        resources :tasks, only: [:index, :create, :update]
       end
     end
   end
